a304cb1170c383a7b8dd412dfacb1e223997666d
HerrLi <ge79cuv@mytum.de>
2023-03-20 00:03:04 +0100

    process code

--- SSN/train.py
+++ SSN/train.py
@@ -50,43 +50,12 @@ train_dataset = EgoDataset(cfg, train_zarr, rasterizer, perturbation)
 val_dataset=EgoDataset(cfg, val_zarr, rasterizer)
 print("train: ",train_dataset)
 print("val: ",val_dataset)
-# plot same example with and without perturbation
-# for perturbation_value in [1, 0]:
-#     perturbation.perturb_prob = perturbation_value
 
-#     data_ego = train_dataset[0]
-#     im_ego = rasterizer.to_rgb(data_ego["image"].transpose(1, 2, 0))
-#     target_positions = transform_points(data_ego["target_positions"], data_ego["raster_from_agent"])
-#     draw_trajectory(im_ego, target_positions, TARGET_POINTS_COLOR)
-#     plt.imshow(im_ego)
-#     plt.axis('off')
-#     plt.show()
-
-# before leaving, ensure perturb_prob is correct
 perturbation.perturb_prob = perturb_prob
 
 
 
 model = MyModel()
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
-# if torch.cuda.device_count()>0:
-#     model=nn.DataParallel(model)
-
-# model = model.to(device)
-
-# print(model)
-
-
-# model = RasterizedPlanningModel(
-#         model_arch="resnet50",
-#         num_input_channels=rasterizer.num_channels(),
-#         num_targets=3 * cfg["model_params"]["future_num_frames"],  # X, Y, Yaw * number of future states,
-#         weights_scaling= [1., 1., 1.],
-#         criterion=nn.MSELoss(reduction="none")
-#         )
 
 
 
@@ -169,13 +138,9 @@ def validate(model, val_dataloader,epoch):
         torch.save(model,"/tmp/"+str(epoch)+"planning.pt")
         return val_loss
 
-#written and saved in train.py
-# lists to store per-epoch loss and accuracy values
-# train_loss = []
-# val_loss = []
-# start = time.time()
+
 n=cfg["train_params"]["max_num_steps"]
-for epoch in range(10):
+for epoch in range(1000):
     print(f"Epoch {epoch+1} of {n}")
     train_epoch_loss = fit(
         model, train_dataloader,optimizer
@@ -183,52 +148,16 @@ for epoch in range(10):
     val_epoch_loss = validate(
         model, val_dataloader,epoch
     )
-    # train_loss.append(train_epoch_loss)
-    # train_accuracy.append(train_epoch_accuracy)
-    # val_loss.append(val_epoch_loss)
-    # val_accuracy.append(val_epoch_accuracy)
-    # if args['lr_scheduler']:
+
     lr_scheduler(val_epoch_loss)
-    # if args['early_stopping']:
+
     early_stopping = EarlyStopping()
     early_stopping(val_epoch_loss)
     if early_stopping.early_stop:
         break
-    # print(f"Train Loss: {train_epoch_loss:.4f}, Train Acc: {train_epoch_accuracy:.2f}")
-    # print(f'Val Loss: {val_epoch_loss:.4f}, Val Acc: {val_epoch_accuracy:.2f}')
-# end = time.time()
+
 print("Finished")
 
-# tr_it = iter(train_dataloader)
-# progress_bar = tqdm(range(cfg["train_params"]["max_num_steps"]))
-# losses_train = []
-# model.train()
-# torch.set_grad_enabled(True)
-
-# for _ in progress_bar:
-#     try:
-#         data = next(tr_it)
-#     except StopIteration:
-#         tr_it = iter(train_dataloader)
-#         data = next(tr_it)
-#     # Forward pass
-#     data = {k: v.to(device) for k, v in data.items()}
-#     # print("data: ",data)
-#     result = model(data)
-#     loss = result["loss"]
-#     print("loss: ",loss)
-#     # loss=result["train_acc1"]
-#     # train_loss=AverageMeter.update1(loss.item(),12)
-#     # Backward pass
-#     optimizer.zero_grad()
-#     loss.backward()
-#     # train_loss.backward()
-#     optimizer.step()
-
-#     losses_train.append(loss.item())
-#     # losses_train.append(train_loss)
-#     progress_bar.set_description(f"loss: {loss.item()} loss(avg): {np.mean(losses_train)}")
-#     # progress_bar.set_description(f"loss: {train_loss} loss(avg): {np.mean(losses_train)}")
 
 
 to_save = torch.save(model,"/tmp/planning.pt")
@@ -236,14 +165,4 @@ to_save = torch.save(model,"/tmp/planning.pt")
 plt.plot(np.arange(len(losses_train)), losses_train, label="train loss")
 plt.legend()
 plt.show()
-
-
-# save_path = "/tmp" #当前目录下
-# early_stopping = EarlyStopping(save_path)
-
-# to_save = torch.save(model,"/tmp/planning.pt")
-
-# path_to_save = f"{gettempdir()}/planning_model.pt"
-# to_save.save(path_to_save)
-# print(f"MODEL STORED at {path_to_save}")
 
